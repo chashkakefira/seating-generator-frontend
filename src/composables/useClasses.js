2bbfa6df137463736ff26c083da1b1d2eb95781d
chashkakefira <danieldzen@yandex.ru>
2025-11-30 20:50:20 +0300

    Add addNewClass and deleteClass methods to useClasses.js

--- src/composables/useClasses.js
+++ src/composables/useClasses.js
@@ -6,10 +6,22 @@ export function useClasses()
     const currentClass = ref(null);
 
     const saveClasses = () => {
-        localStorage.setItem('Classes', classes);
+        localStorage.setItem('Classes', JSON.stringify(classes));
     };
     const loadClass = () => {
         const saved = localStorage.getItem('Classes');
         classes.value = saved ? JSON.parse(saved) : [];
+    };
+    const addNewClass = (name) => {
+        const newClass = { id: Date.now(), name, students: [], preferences: [[]], forbidden: [[]]};
+        classes.value.push(newClass);
+        saveClasses();
+    };
+    const deleteClass = (id) => {
+        const idx = classes.value.findIndex(cls => cls.id === id);
+        if (idx !== -1) {
+            classes.value.splice(idx, 1);
+            saveClasses();
+        }
     };
 }
