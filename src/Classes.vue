7794f44531cbed99ef12668d09065b666f8c3f41
chashkakefira <danieldzen@yandex.ru>
2025-11-30 09:49:27 +0300

    Add vue-router
    To handle separate page for entering class data

---
+++ src/Classes.vue
@@ -0,0 +1,1 @@
+<template>Страница классов</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from './App.vue';
+import App from './App.vue'
+import Home from './Home.vue';
+import Classes from './Classes.vue'
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: Home},
+        {path: '/classes', component: Classes}
+    ]
+})
 const app = createApp(App);
+app.use(router);
 app.mount('#app');
